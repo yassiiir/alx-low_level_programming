123a21ba061371a6bf3c5e155370153328c50f76
yassiiir <oubourhim.yassir@gmail.com>
2023-07-24 17:39:30 -0700

    how are you

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,6 +12,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -28,5 +29,4 @@ int main(void)
 	printf("%d is zero\n", n);
 	}
 	return (0);
-}
-			 
+}		 
