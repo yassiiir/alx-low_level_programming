bcc84b6c916c489ba0d42c9f469bec7d27cb85b0
yassiiir <oubourhim.yassir@gmail.com>
2023-08-21 17:37:10 -0700

    next

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,120 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_strings - entry point
- * @format: size of triangle
- * Description: --
- * Return: --
+ * print_all - Prints all of the arguments when specified :)
+ * @format: specifies the necessary operations
+ * Return: void
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	char *s;
-	unsigned int i;
 
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-		return (ptr);
-	}
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	s = malloc(new_size);
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; i < old_size && new_size > old_size; i++)
-		s[i] = *((char *)ptr + i);
-	for (i = 0; i < new_size && new_size < old_size; i++)
-		s[i] = *((char *)ptr + i);
-	free(ptr);
-	return (s);
-}
-void print_char(void a)
+void print_all(const char * const format, ...)
 {
-	printf("%c", a);
-}
-void print_int(void a)
-{
-	printf("%d", a);
-}
-void print_float(void a)
-{
-	printf("%f", a);
-}
-void print_string(void *a)
-{
-	printf("%s", a);
-}
+	int i;
+	int flag;
+	char *str;
+	va_list a_list;
 
-void print_all(const char *const format, ...)
-{
-	int n = 0, i = 0;
-	char *frm = NULL;
-	va_list args;
-	int (*print_type[4])(void) = {print_char, print_int, print_float, print_string};
-
-	while (format[i])
-	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-		{
-			n++;
-			frm = _realloc(frm, n, n + 1);
-			if (!frm)
-			{
-				return;
-			}
-			frm[n - 1] = format[i];
-		}
-		i++;
-	}
-	va_start(args, n);
-	frm[n] = '\0';
+	va_start(a_list, format);
 	i = 0;
-	while (frm[i])
+	while (format != NULL && format[i] != '\0')
 	{
-		print_type[i];
-		if (i != i - 1)
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
 		i++;
 	}
-	printf("%d\n", n);
-	printf("%s\n", frm);
-}
-/*
-get format
-	check if it have c i f s
-		count how many of cifs exist
-			that number is what gonna tell us how many argument we have
-
-	2 while 2 if 9 variable
-		filter format and put everything in array
-		now that i have n
-			loop through args
-				and print it acording to array format index
-	/n at end
-
-
-*/
-/*
-	va_list args;
-	int i = 0;
-	char *s;
-
-	va_start(args, n);
-	for (i = 0; i < (int)n; i++)
-	{
-		s = va_arg(args, char *);
-		printf("%s%s",
-			   s == NULL ? "(nil)" : s,
-			   i != (int)n - 1 && separator != NULL ? separator : "");
-	}
-	va_end(args);
 	printf("\n");
-
-*/
+	va_end(a_list);
+}
